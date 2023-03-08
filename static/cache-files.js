f0ec34d7a01b5d229affdb8a4055d3ea158c1708
Lee Kierstead <contact@leecalvink.net>
2023-03-07 20:27:38 -0700

    Trying Offline
    Set up service worker to cache fonts

--- static/cache-files.js
+++ static/cache-files.js
@@ -1,10 +1,18 @@
 // Define the files to be cached
 const filesToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/images/logo.png'
+  '/main.css',
+  '/fonts/equity/regular.woff2',
+  '/fonts/equity/bold.woff2',
+  '/fonts/equity/italic.woff2',
+  '/fonts/advocate/54_wide_reg.woff2',
+  '/fonts/advocate/55_wide_med.woff2',
+  '/fonts/advocate/56_wide_bold.woff2',
+  '/fonts/advocate/slab_54_wide_reg.woff2',
+  '/fonts/advocate/slab_55_wide_med.woff2',
+  '/fonts/advocate/slab_56_wide_bold.woff2',
+  '/fonts/concourse/6_semibold.woff2',
+  '/fonts/concourse/4_medium.woff2',
+  '/fonts/concourse/4_medium_italic.woff2'
 ];
 
 // Define the cache version
